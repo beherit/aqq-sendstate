24710856ebc5b785a03b8b3b93a0c72bc1607ad7
Krzysztof Grochocki <Krzysztof Grochocki@localhost>
2013-07-28 01:43:14 +0200

    * Wys?any opis ustawiany jest w ko?cu na ca?y czas trwania sesji! * Oznaczenie wys?ania ju? statusu do kontaktu. * Mo?liwo?? szybkiego przywr?cenia aktualnego opisu. * Przepisanie wtyczki pod kompilator Embarcadero C++ Builder XE3. * Aktualizacja komponent?w AlphaControls do najnowszej dost?pnej wersji (7.70). * Optymalizacja kodu.

--- SendFrm.cpp
+++ SendFrm.cpp
@@ -13,15 +13,17 @@
 #pragma link "sComboBoxes"
 #pragma link "acAlphaImageList"
 #pragma link "sComboBox"
+#pragma link "sSpeedButton"
 #pragma resource "*.dfm"
 TSendForm *SendForm;
 //---------------------------------------------------------------------------
 __declspec(dllimport)UnicodeString GetThemeSkinDir();
 __declspec(dllimport)bool ChkSkinEnabled();
 __declspec(dllimport)bool ChkNativeEnabled();
-__declspec(dllimport)void SendXML(UnicodeString JID, int State, UnicodeString Status, int UserIdx);
+__declspec(dllimport)void SendXML(UnicodeString JID, int UserIdx, UnicodeString Status, int State);
 __declspec(dllimport)UnicodeString GetIconPath(int Icon);
-bool IconsChange = false;
+__declspec(dllimport)UnicodeString GetStatus(int UserIdx);
+__declspec(dllimport)int GetState(int UserIdx);
 //---------------------------------------------------------------------------
 __fastcall TSendForm::TSendForm(TComponent* Owner)
 	: TForm(Owner)
@@ -31,20 +33,23 @@ __fastcall TSendForm::TSendForm(TComponent* Owner)
 
 void __fastcall TSendForm::aExitExecute(TObject *Sender)
 {
-
+  //Zamkniecie formy
   Close();
 }
 //---------------------------------------------------------------------------
 
 void __fastcall TSendForm::aSelectMemoExecute(TObject *Sender)
 {
+  //Zaznaczenie calego tekstu
   StatusMemo->SelectAll();
 }
 //---------------------------------------------------------------------------
 
 void __fastcall TSendForm::SendButtonClick(TObject *Sender)
 {
-  SendXML(JID,StateComboBox->ItemIndex,StatusMemo->Text.Trim(),UserIdx);
+  //Wyslanie nowego statusu
+  SendXML(JID,UserIdx,StatusMemo->Text.Trim(),StateComboBox->ItemIndex);
+  //Zamkniecie formy
   Close();
 }
 //---------------------------------------------------------------------------
@@ -66,7 +71,20 @@ void __fastcall TSendForm::FormShow(TObject *Sender)
 	else
 	 sSkinManager->Active = false;
   }
-  StateComboBox->SetFocus();
+  //Wczytanie ikon z interfesju AQQ
+  sAlphaImageList->AcBeginUpdate();
+  sAlphaImageList->Clear();
+  sAlphaImageList->LoadFromFile(GetIconPath(1));
+  sAlphaImageList->LoadFromFile(GetIconPath(7));
+  sAlphaImageList->LoadFromFile(GetIconPath(3));
+  sAlphaImageList->LoadFromFile(GetIconPath(4));
+  sAlphaImageList->LoadFromFile(GetIconPath(5));
+  sAlphaImageList->LoadFromFile(GetIconPath(2));
+  sAlphaImageList->LoadFromFile(GetIconPath(49));
+  //Ustawienie ikonki na przycisku przywracania statusu
+  ResetButton->Images = sAlphaImageList;
+  ResetButton->ImageIndex = 6;
+  //Ustawianie fokusa na polu wpisywania opisu
   StatusMemo->SetFocus();
   StatusMemo->SelectAll();
 }
@@ -95,28 +113,19 @@ void __fastcall TSendForm::FormCreate(TObject *Sender)
 void __fastcall TSendForm::StateComboBoxDrawItem(TWinControl *Control, int Index, TRect &Rect,
 		  TOwnerDrawState State)
 {
-  if(!IconsChange)
-  {
-    sAlphaImageList->AcBeginUpdate();
-	sAlphaImageList->Clear();
-	sAlphaImageList->LoadFromFile(GetIconPath(1));
-	sAlphaImageList->LoadFromFile(GetIconPath(7));
-	sAlphaImageList->LoadFromFile(GetIconPath(3));
-	sAlphaImageList->LoadFromFile(GetIconPath(4));
-	sAlphaImageList->LoadFromFile(GetIconPath(5));
-	sAlphaImageList->LoadFromFile(GetIconPath(2));
-	IconsChange = true;
-  }
+  //Dodanie ikonek do ComboBox
   if(State.Contains(odSelected)) StateComboBox->Canvas->DrawFocusRect(Rect);
   StateComboBox->Canvas->Brush->Style = bsClear;
   sAlphaImageList->Draw(StateComboBox->Canvas,Rect.left+2,Rect.top+2,Index);
-  StateComboBox->Canvas->TextOutA(Rect.left+22,Rect.top+3,StateComboBox->Items->Strings[Index]);
+  StateComboBox->Canvas->TextOutW(Rect.left+22,Rect.top+3,StateComboBox->Items->Strings[Index]);
 }
 //---------------------------------------------------------------------------
 
-void __fastcall TSendForm::FormPaint(TObject *Sender)
+void __fastcall TSendForm::ResetButtonClick(TObject *Sender)
 {
-  IconsChange = false;	
+  //Przywracanie aktualnego opisu
+  StatusMemo->Text = GetStatus(UserIdx);
+  //Przywracanie aktualnego statusu
+  StateComboBox->ItemIndex = GetState(UserIdx);
 }
 //---------------------------------------------------------------------------
-
